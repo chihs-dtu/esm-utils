6c6a1c8083ac1e1448bed159637e4cd08d94508e
chihs-dtu <chihs@dtu.dk>
2025-02-06 14:45:07 +0100

    add test

---
+++ test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+INPUT_FILE="example_proteins/example_proteins.fasta"
+DIR_NAME="example_proteins/"
+# Check for example files
+if [ ! -f "$INPUT_FILE" ]; then
+  echo "Error: Example protein FASTA file not found."
+  exit 1
+fi
+
+# Run the script
+conda activate esm2
+if python run.py $INPUT_FILE; then
+	# Check the output files
+	for filename in esm2enc attention; do
+		if [ -f "${DIR_NAME}/${filename}.pickle" ]; then
+			# Check the dimension
+			if python -c '
+import json
+obj = json.load(open("${DIR_NAME}/${filename}}.json","r"))
+if len(obj)!=5: 
+	exit(1)
+else 
+	exit(0)
+';
+			then
+				echo "Error: The output pickle file has incorrect dimension."
+				exit 1
+			fi
+		fi
+	done
+	echo "Success!"
+else
+	echo "Error: Failed to run the script."
+fi
